80bd9bd3d9b75356450e2b5859b76f1ceb5246dd
D4riu5 <R3iji94@gmail.com>
2023-02-20 16:21:04 +0100

    V 0.1 - added: printend the $hotels values on page - added; bootstrap

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP hotels</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+</head>
+<body class="bg-dark text-white">
+    
+<?php
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+?>
+
+<?php
+    foreach ($hotels as $hotel) {
+        echo '<div class="">';
+        echo '<h2>' . $hotel['name'] . '</h2>';
+        echo $hotel['description'] . '<br>';
+        echo ($hotel['parking'] ? 'does have parking' : 'does not have parking') . '<br>';
+        echo 'vote ' . $hotel['vote'] . '<br>';
+        echo 'distance from center = ' . $hotel['distance_to_center'] . ' km<br><br>';
+        echo '</div>';
+    }
+?>
+
+
+</body>
+</html>
