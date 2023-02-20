b120bf011e4ce35eb9c5fab2575f6c58b1ea0dc9
D4riu5 <R3iji94@gmail.com>
2023-02-20 17:10:04 +0100

    V 0.4 - added: select input inside the form that filters hotels based on votes. (after submitting the form, the values dont reset for both select and checkbox)

--- index.php
+++ index.php
@@ -10,87 +10,100 @@
 </head>
 <body class="bg-dark text-white">
     
-<?php
-$hotels = [
+    <?php
+        $hotels = [
+                [
+                    'name' => 'Hotel Belvedere',
+                    'description' => 'Hotel Belvedere Descrizione',
+                    'parking' => true,
+                    'vote' => 4,
+                    'distance_to_center' => 10.4
+                ],
+                [
+                    'name' => 'Hotel Futuro',
+                    'description' => 'Hotel Futuro Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 2
+                ],
+                [
+                    'name' => 'Hotel Rivamare',
+                    'description' => 'Hotel Rivamare Descrizione',
+                    'parking' => false,
+                    'vote' => 1,
+                    'distance_to_center' => 1
+                ],
+                [
+                    'name' => 'Hotel Bellavista',
+                    'description' => 'Hotel Bellavista Descrizione',
+                    'parking' => false,
+                    'vote' => 5,
+                    'distance_to_center' => 5.5
+                ],
+                [
+                    'name' => 'Hotel Milano',
+                    'description' => 'Hotel Milano Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 50
+                ],
 
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
+            ];
+    ?>
 
-];
-?>
-
-<div class="container">
-  <h1 class="my-3">Hotel List</h1>
-  <form method="get" class="d-flex align-items-center">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" name="parking" id="parking" <?php if (isset($_GET['parking']) && $_GET['parking'] == 'on') echo 'checked'; ?>>
-      <label class="form-check-label" for="parking">
-        Only show hotels with parking
-      </label>
+    <div class="container">
+    <h1 class="my-3">Hotel List</h1>
+    <form method="get" class="d-flex flex-column my-3">
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="parking" id="parking" <?php if (isset($_GET['parking']) && $_GET['parking'] == 'on') echo 'checked'; ?>>
+            <label class="form-check-label" for="parking">
+                Only show hotels with parking
+            </label>
+        </div>
+        <div class="d-flex align-items-center my-2">
+            <label class="me-2" for="vote">Filter by vote:</label>
+            <select class="form-control w-25" id="vote" name="vote">
+                <option value="all" <?php if (!isset($_GET['vote']) || $_GET['vote'] == 'all') echo 'selected'; ?>>All</option>
+                <option value="1" <?php if (isset($_GET['vote']) && $_GET['vote'] == '1') echo 'selected'; ?>>1</option>
+                <option value="2" <?php if (isset($_GET['vote']) && $_GET['vote'] == '2') echo 'selected'; ?>>2</option>
+                <option value="3" <?php if (isset($_GET['vote']) && $_GET['vote'] == '3') echo 'selected'; ?>>3</option>
+                <option value="4" <?php if (isset($_GET['vote']) && $_GET['vote'] == '4') echo 'selected'; ?>>4</option>
+                <option value="5" <?php if (isset($_GET['vote']) && $_GET['vote'] == '5') echo 'selected'; ?>>5</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary my-2 w-25">Filter</button>
+    </form>
+    <table class="table text-white">
+        <thead>
+            <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Parking</th>
+            <th>Vote</th>
+            <th>Distance from Center (km)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach ($hotels as $hotel) {
+                if (isset($_GET['parking']) && !$hotel['parking']) {
+                    continue;
+                }
+                if (isset($_GET['vote']) && $_GET['vote'] != 'all' && $hotel['vote'] != $_GET['vote']) {
+                    continue;
+                }
+                    echo '<tr>';
+                    echo '<td>' . $hotel['name'] . '</td>';
+                    echo '<td>' . $hotel['description'] . '</td>';
+                    echo '<td>' . ($hotel['parking'] ? 'Yes' : 'No') . '</td>';
+                    echo '<td>' . $hotel['vote'] . '</td>';
+                    echo '<td>' . $hotel['distance_to_center'] . '</td>';
+                    echo '</tr>';
+                    }
+            ?>
+        </tbody>
+    </table>
     </div>
-    <button type="submit" class="btn btn-primary mx-2">Filter</button>
-  </form>
-  <table class="table text-white">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Parking</th>
-        <th>Vote</th>
-        <th>Distance from Center (km)</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      foreach ($hotels as $hotel) {
-        if (isset($_GET['parking']) && !$hotel['parking']) {
-            continue;
-          }
-        echo '<tr>';
-        echo '<td>' . $hotel['name'] . '</td>';
-        echo '<td>' . $hotel['description'] . '</td>';
-        echo '<td>' . ($hotel['parking'] ? 'Yes' : 'No') . '</td>';
-        echo '<td>' . $hotel['vote'] . '</td>';
-        echo '<td>' . $hotel['distance_to_center'] . '</td>';
-        echo '</tr>';
-      }
-      ?>
-    </tbody>
-  </table>
-</div>
 
 
 
